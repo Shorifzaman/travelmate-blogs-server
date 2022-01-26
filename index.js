c5f030c76af09b9dac26b58a4fdf812168b11fb6
shorifzaman <shorif.zaman@gmail.com>
2022-01-26 23:22:05 +0600

    using git for heroku deployment

--- index.js
+++ index.js
@@ -1,50 +1,154 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require("path");
-require("dotenv").config();
-
-//config Express App
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors')
+require('dotenv').config();
 const app = express();
-app.use(express.json());
+
+const port = process.env.PORT || 4000;
+
+
+// middleware-------------
 app.use(cors());
+app.use(express.json());
 
-//config PORT
-const PORT = process.env.PORT || 5000;
 
-//config MongoDB
-const uri = process.env.MONGO_URI;
-console.log("url link", uri);
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-});
-const connection = mongoose.connection;
-connection.once("open", () =>
-    console.log("MongoDB connection has been established!")
-);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.af4at.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-//config routes
-const postsRouter = require("./routes/posts");
-const authRouter = require("./routes/auth");
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// ------------------------------------------------------
 
-app.use("/auth", authRouter);
-// To differentiate backend posts route, I am adding server/
-app.use("/server/posts", postsRouter);
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("travelmate");
+        const travelsCollection = database.collection("travelpost");
+        const usersCollection = database.collection('users');
+        // console.log(database)
 
-//Load the npm build package of the frontend CRA
-if (process.env.NODE_ENV === "production") {
-    // set a static folder
-    app.use(express.static("frontend/build"));
+        // GET all services----------------------
+        app.get('/travels', async (req, res) => {
+            const travels = await travelsCollection.find({}).toArray();
+            res.send(travels);
+        })
+        app.post('/travelPost', async (req, res) => {
+            const service = req.body;
+            const result = await travelsCollection.insertOne(service)
+            res.send(result)
+        })
 
-    // Provide a wildcard as a fallback for all routes
-    app.get("*", (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, "../frontend", "build", "index.html")
-        );
-    });
+        //get travel--------------------
+        app.get('/travel/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const travel = await travelsCollection.findOne(query)
+            res.send(travel)
+        })
+        // update travel-------------------
+        app.put("/UpdateTravelsFrom/:id", async (req, res) => {
+            const id = req.params.id;
+            const updateTravel = req.body;
+            const filter = { _id: ObjectId(id) };
+
+            travelsCollection.updateOne(filter, {
+                $set: {
+                    title: updateTravel?.title,
+                    description: updateTravel?.description,
+                    price: updateTravel?.price,
+                    img: updateTravel?.img,
+                    category: updateTravel?.category,
+                    info: updateTravel?.info,
+                    address: updateTravel?.address,
+                    rating: updateTravel?.rating
+                },
+            })
+                .then((result) => {
+                    res.send(result);
+                });
+        });
+
+        // delete travels ===========================
+        app.delete('/travelDelete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await travelsCollection.deleteOne(query);
+            res.send(result)
+
+        })
+        // get admin-----------------------
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAmin = false;
+            if (user?.role === "admin") {
+                isAmin = true;
+            }
+            res.json({ admin: isAmin })
+        })
+
+        // approve api-------------------
+        app.put("/approve/:id", async (req, res) => {
+            const id = req.params.id;
+            const role = req.body.role;
+            console.log(role)
+            const filter = { _id: ObjectId(id) };
+            await travelsCollection.updateOne(filter, {
+                $set: {
+                    role: role,
+
+                },
+            })
+                .then((result) => {
+                    res.send(result);
+                });
+
+        });
+
+        // save to database user --------------
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.json(result)
+            // console.log(user)
+        })
+        // update user=======================
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+
+        })
+
+        // admin add -------------------- verifyToken,
+        app.put('/users/admin', async (req, res) => {
+
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } }
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result)
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
 }
 
-//Host app at PORT
-app.listen(PORT, () => console.log(`Server is running at PORT ${PORT}!`));
+run().catch(console.dir);
+// ------------------------------------------------------
+
+
+app.get('/', (req, res) => {
+    res.send('Explore travel running');
+})
+
+app.listen(port, () => {
+    console.log('Explore travel running', port)
+})
+// https://polar-ocean-68780.herokuapp.com/ | https://git.heroku.com/polar-ocean-68780.git
